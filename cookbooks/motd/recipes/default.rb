ae92aad5ad710eae020acf4be295353204286a12
Rahul Aggrawal <rahulaggrawal50@gmail.com>
2018-04-21 23:38:18 -0700

    mpty repo

---
+++ cookbooks/motd/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: motd
+# Recipe:: default
+#
+# Copyright 2018, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file '/etc/motd' do
+	 content 'Welcome to Chef'
+end
